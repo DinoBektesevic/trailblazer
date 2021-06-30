06a6c7f890bd8871984cc3b64480e5b411ae030e
DinoBektesevic <dinob@uw.edu>
2021-06-29 19:44:47 -0700

    Fix authentication temporarilyto see CI work.

--- trail/trail/settings.py
+++ trail/trail/settings.py
@@ -15,8 +15,7 @@ import os
 
 from .config import DbAuth, SiteConfig
 
-siteConfig = SiteConfig.fromYaml()
-dbConfig = DbAuth.fromYaml()
+siteConfig = SiteConfig({"secret_key": "alalal"})
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
