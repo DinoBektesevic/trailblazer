cab07da0712093ea8ac6de27617b17d7773ecbde
DinoBektesevic <dinob@uw.edu>
2021-06-29 19:45:25 -0700

    Remove the original trailblazer.

--- trailblazer/upload/jQueryUpload/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Photo(models.Model):
-    title = models.CharField(max_length=255, blank=True)
-    file = models.FileField(upload_to='static/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
--- trailblazer/upload/jQueryUpload/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.BasicUploadView.as_view(), name='upload'),
-]
